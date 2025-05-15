d55cd245652eebf40f18135445092621e7d0f619
alishercoder <alishersharipov670@gmail.com>
2025-05-16 02:12:34 +0500

    update

--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -1,14 +1,15 @@
 import { useFetch } from "@/hooks/useFetch";
 import React from "react";
 
-const Genres = ({ setGenre, genre }) => {
+const Genres = ({ handleChangeGenre, genres }) => {
    const { data } = useFetch("/genre/movie/list");
+   genres = genres.split("-").slice(1);
 
    return (
       <div className="container mx-auto py-4">
          <ul className="text-[#fff] flex gap-2 overflow-auto py-1 no-scrollbar">
             {data?.genres?.map((gen) => (
-               <li onClick={() => setGenre(gen.id.toString())} className={`text-nowrap text-text cursor-pointer select-none bg-[#1D1D1D] px-2 py-1.5 rounded-[12px] ${genre == gen.id ? "bg-primary text-white" : "bg-[#1D1D1D] hover:bg-[#333]"} `} key={gen.id}>
+               <li onClick={() => handleChangeGenre(gen.id.toString())} className={`text-nowrap text-text cursor-pointer select-none bg-[#1D1D1D] px-2 py-1.5 rounded-[12px] ${genres.includes(gen.id.toString()) ? "bg-primary text-white" : "bg-[#1D1D1D] hover:bg-[#333]"} `} key={gen.id}>
                   {gen.name}
                </li>
             ))}
--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,15 +1,53 @@
 import Genres from "@/components/genres/Genres";
 import MovieView from "@/components/movie-view/MovieView";
 import { useFetch } from "@/hooks/useFetch";
-import React, { useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { useCallback } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Movies = () => {
-   const [genre, setGenre] = useState("");
-   const { data, error, loading } = useFetch("/discover/movie", { page: 1, with_genres: genre, without_genres: "18, 10749, 36, 27, 99" });
+   const [params, setParams] = useSearchParams();
+   let page = params.get("page") || 1;
+   let genres = params.get("genres") || "";
+   let with_genres = genres.split("-").join(",").slice(1);
+
+   const { data, error, loading } = useFetch("/discover/movie", { page, with_genres, without_genres: "18, 10749, 36, 27, 99" });
+
+   const handleChange = (event, value) => {
+      if (value == 1) {
+         params.delete("page");
+      } else {
+         params.set("page", value.toString());
+      }
+      setParams(params);
+   };
+
+   const handleChangeGenre = useCallback((id) => {
+      let array = genres.split("-");
+      if (array.includes(id)) {
+         genres = array.filter((i) => i !== id).join("-");
+      } else {
+         genres += `-${id}`;
+      }
+
+      params.set("page", "1");
+      if (!genres) {
+         params.delete("genres");
+         params.delete("pages");
+      } else {
+         params.set("genres", genres);
+      }
+
+      setParams(params);
+   }, []);
+
    return (
       <div className="mb-[40px]">
-         <Genres setGenre={setGenre} genre={genre} />
-         <MovieView movies={data?.results} />
+         <Genres genres={genres} handleChangeGenre={handleChangeGenre} />
+         {loading ? <div className="w-20 mx-auto">Skleton.....</div> : <MovieView movies={data?.results} />}
+         <div className="container mx-auto flex justify-center my-10 w-full bg-dark py-2 rounded-[12px]">
+            <Pagination page={Number(page)} count={data?.total_pages > 500 ? 500 : data?.total_pages} onChange={handleChange} color="primary" />
+         </div>
       </div>
    );
 };
